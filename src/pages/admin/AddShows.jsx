3bd6d0e41cbbd9b3f61e2759b734cfd8639bfc8a
AbhinavKV369 <abhinavkvmayyil@gmail.com>
2025-10-11 18:04:12 +0530

    Added onClick property

--- src/pages/admin/AddShows.jsx
+++ src/pages/admin/AddShows.jsx
@@ -22,9 +22,9 @@ const AddShows = () => {
       <p className="mt-6 text-lg font-semibold mx-2">Now Playing Movies</p>
       <div className="mt-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 px-2">
         {dummyShowsData.map((movie) => (
-          <div
+          <div onClick={setSelectedMovie(movie)}
             key={movie._id}
-            className="bg-zinc-900 cursor-pointer rounded-lg overflow-hidden border-2 border-transparent hover:border-amber-400 transition-all hover:scale-105">
+            className="bg-zinc-900 cursor-pointer rounded-lg overflow-hidden border-2 border-transparent hover:border-red-600 transition-all hover:scale-105">
             <img
               src={movie.poster_path}
               alt={movie.title}
@@ -38,22 +38,22 @@ const AddShows = () => {
       </div>
 
       {/* Add Show Form */}
-      <div className="mt-10 bg-zinc-800/70 p-6 rounded-xl border border-amber-400/40 backdrop-blur-md">
+      <div className="mt-10 bg-zinc-800/70 p-6 rounded-xl border-2 border-red-400/40 backdrop-blur-md">
         <h3 className="text-lg font-semibold mb-4">
-          Add Show for: <span className="text-amber-400">Selected Movie</span>
+          Add Show for: <span className="text-red-600">Selected Movie</span>
         </h3>
 
         <div className="flex flex-col md:flex-row gap-4">
           <input
             type="datetime-local"
-            className="bg-zinc-900 text-white p-3 rounded-md border border-zinc-700 w-full focus:outline-none focus:border-amber-400"
+            className="bg-white/70 text-black accent-red-600  p-3 rounded-md border border-zinc-700 w-full focus:outline-none focus:border-red-700 "
           />
           <input
             type="number"
             placeholder="Enter ticket price"
-            className="bg-zinc-900 text-white p-3 rounded-md border border-zinc-700 w-full focus:outline-none focus:border-amber-400"
+            className="bg-white/70 text-black p-3 rounded-md border border-zinc-700 w-full focus:outline-none focus:border-red-700"
           />
-          <button className="bg-amber-500 text-black font-semibold px-6 py-3 rounded-md hover:bg-amber-400 transition">
+          <button className="bg-red-500 text-white font-semibold px-6 py-3 rounded-md hover:bg-red-600 transition">
             Add Show
           </button>
         </div>
